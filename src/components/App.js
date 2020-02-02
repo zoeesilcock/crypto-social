2df93392873d31ef7bafda4be446c7d17fc962d2
Zoee Silcock <mrzoee@gmail.com>
2020-02-02 10:26:16 +0100

    Connect to the users ethereum wallet

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
+
 import './App.css';
+import Navbar from './Navbar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      account: '',
+    }
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3();
+
+    if (window.web3) {
+      await this.loadBlockchainData();
+    }
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('Non-Ethereum browser detected. Please install Metamask or use Brave.');
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ account: accounts[0] });
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CryptoSocial
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
